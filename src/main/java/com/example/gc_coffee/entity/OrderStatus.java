5b1f2b79ff3c1c9ea1219554c2a71c7a456f6132
MisaSohee <sohee5951@naver.com>
2024-09-09 16:33:33 +0900

    feat: OrderStatus에 메시지 추가

--- src/main/java/com/example/gc_coffee/entity/OrderStatus.java
+++ src/main/java/com/example/gc_coffee/entity/OrderStatus.java
@@ -1,9 +1,18 @@
 package com.example.gc_coffee.entity;
 
 public enum OrderStatus {
-    ORDERED,
-    PAYMENT_PENDING,
-    CONFIRMED,
-    SHIPPED,
-    DELIVERED;
+    ORDERED("주문이 접수되었습니다."),
+    PAYMENT_PENDING("결제가 진행 중입니다."),
+    CONFIRMED("당일 오후 2시 이후의 주문은 다음날 배송을 시작합니다."),
+    SHIPPED("주문이 발송되었습니다."),
+    DELIVERED("주문이 당일 배송됩니다.");
+    private final String message;
+
+    OrderStatus(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
