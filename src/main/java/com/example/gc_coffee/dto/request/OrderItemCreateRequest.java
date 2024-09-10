c7b2eda3660897e58a37067bf5df40023d7e780b
MisaSohee <sohee5951@naver.com>
2024-09-10 15:05:26 +0900

    style: OrderItemCreateRequest의 import문 정리

--- src/main/java/com/example/gc_coffee/dto/request/OrderItemCreateRequest.java
+++ src/main/java/com/example/gc_coffee/dto/request/OrderItemCreateRequest.java
@@ -1,6 +1,5 @@
 package com.example.gc_coffee.dto.request;
 
-import com.example.gc_coffee.entity.ProductCategory;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
