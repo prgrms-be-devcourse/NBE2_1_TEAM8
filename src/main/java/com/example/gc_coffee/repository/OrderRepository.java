47ce49c509a4ff61a2bd14a636662abc5d9f7110
MisaSohee <sohee5951@naver.com>
2024-09-10 15:03:36 +0900

    feat: OrderRepository에 findAllByEmail 메서드 생성

--- src/main/java/com/example/gc_coffee/repository/OrderRepository.java
+++ src/main/java/com/example/gc_coffee/repository/OrderRepository.java
@@ -3,8 +3,10 @@ package com.example.gc_coffee.repository;
 import com.example.gc_coffee.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Optional<Order> findByEmail(String email);
+    List<Order> findAllByEmail(String email);
 }
