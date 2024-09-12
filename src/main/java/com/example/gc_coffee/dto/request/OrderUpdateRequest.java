e7ecc63b7214e84ef5378d088258a95a51f0ec09
josh971230 <brionlee97@naver.com>
2024-09-12 10:55:32 +0900

    docs: API 명세서 작성

--- src/main/java/com/example/gc_coffee/dto/request/OrderUpdateRequest.java
+++ src/main/java/com/example/gc_coffee/dto/request/OrderUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.example.gc_coffee.dto.request;
 
 import com.example.gc_coffee.entity.Order;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +13,11 @@ import java.util.Map;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "주문 항목 수량 수정 요청")
 public class OrderUpdateRequest {
 
-    // 수정할 주문 항목의 ID와 수량
+    @Schema(description = "수정할 주문 항목 ID와 그 항목의 새로운 수량을 설정합니다. (주문 항목 0으로 변경 시 삭제)")
+
     private Map<Long, Integer> itemQuantityUpdates;
 
 
